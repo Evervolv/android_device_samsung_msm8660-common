b24dac3102d1e713336dabe1007d9d59b628999c
bryan2894 <bryan2894@gmail.com>
2016-10-07 21:45:37 -0400

    msm8660: disable dexpreopt
    Change-Id: Ie07f1c9ffc9889f3326109b4b2f9212a4fdd7cf7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,19 +50,6 @@ TARGET_NEEDS_TEXT_RELOCATIONS := true
 BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 
-# Dex-preoptimization
-ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-    WITH_DEXPREOPT_COMP := false
-    ifneq ($(TARGET_BUILD_VARIANT),user)
-      # Retain classes.dex in APK's for non-user builds
-      DEX_PREOPT_DEFAULT := nostripping
-    endif
-  endif
-endif
-DONT_DEXPREOPT_PREBUILTS := true
-
 # Display
 BOARD_USES_LEGACY_MMAP := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
