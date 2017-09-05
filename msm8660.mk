aacea2102322dc8818700ecb7ac3eafd7048c548
Nick Reuter <nreuter85@gmail.com>
2017-09-05 00:11:29 -0500

    msm8660: update nfcee_access.xml

--- msm8660.mk
+++ msm8660.mk
@@ -227,13 +227,8 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
 PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # OMX
 PRODUCT_PACKAGES += \
